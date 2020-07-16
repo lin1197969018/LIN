6048438ddf890cb10b2a4ae64ae7dbe2a9a2a506
Your Name <you@example.com>
2020-07-16 19:51:42 +0800

    first commit

--- src/main/java/com/example/demo716/Demo1.java
+++ src/main/java/com/example/demo716/Demo1.java
@@ -2,6 +2,6 @@ package com.example.demo716;
 
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("tes!t");
     }
 }
