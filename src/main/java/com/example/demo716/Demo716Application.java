1dca5706e91e4f7af64cc9c375b2e358db43c93e
Your Name <you@example.com>
2020-07-16 19:43:55 +0800

    first commit

---
+++ src/main/java/com/example/demo716/Demo716Application.java
@@ -0,0 +1,13 @@
+package com.example.demo716;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo716Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo716Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo716/Demo716ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo716;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo716ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
